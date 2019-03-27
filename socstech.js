ed49d17c73eae7777113a2b8afde8442496c0dd0
Tom Reed <t.reed1104@gmail.com>
2019-03-27 17:21:09 +0000

    ReImplemented the header bar as a vue component

--- socstech.js
+++ socstech.js
@@ -1,16 +1,29 @@
 Vue.component('navbar-items', {
     props: ['item'],
     template: "<a class='nav-item' href=''>{{ item.text }}</a>"
-});
+})
 
-var navigationBar = new Vue({
-    el: '#navbar',
-    data: {
-        items: [
-            { link: 'index.html', text: 'Home' },
-            { link: 'guides.html', text: 'Guides' },
-            { link: 'resources.html', text: 'Resources' },
-            { link: 'contact.html', text: 'Contact Us' }
-        ]
-    }
-});
+Vue.component('header-component', {
+    props: ['name'],
+    data: function () {
+        return {
+            items: [
+                { link: 'index.html', text: 'Home' },
+                { link: 'guides.html', text: 'Guides' },
+                { link: 'resources.html', text: 'Resources' },
+                { link: 'contact.html', text: 'Contact Us' }
+            ]
+        }
+    },
+    template: `
+    <div class='header'>
+        <h1 class='header-title'> {{ name }} </h1>
+        <div class='nav-bar'>
+            <navbar-items v-for="item in items" v-bind:item="item" v-bind:href="item.link"></navbar-items>
+        </div>
+    </div>`
+})
+
+var app = new Vue({
+    el: '#app',
+})
