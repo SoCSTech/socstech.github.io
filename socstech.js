b920e9dd97651a27694d440a76959b6ec9f500d1
Tom Reed <t.reed1104@gmail.com>
2019-03-28 11:18:45 +0000

    Added the js for dynamically finding out what the active page is in order to toggle the .active css class

--- socstech.js
+++ socstech.js
@@ -6,20 +6,33 @@ Vue.component('navbar-items', {
 Vue.component('header-component', {
     props: ['name'],
     data: function () {
-        return {
-            items: [
-                { link: 'index.html', text: 'Home' },
-                { link: 'guides.html', text: 'Guides' },
-                { link: 'resources.html', text: 'Resources' },
-                { link: 'contact.html', text: 'Contact Us' }
-            ]
-        }
+        // Our array of the items to render in the navigation bar
+        var menuItems = [
+            { link: 'index.html', text: 'Home', isActive: false },
+            { link: 'guides.html', text: 'Guides', isActive: false },
+            { link: 'resources.html', text: 'Resources', isActive: false },
+            { link: 'contact.html', text: 'Contact Us', isActive: false }
+        ];
+
+        // Get the page name for finding out the active page
+        var pageName = document.location.pathname;
+        pageName = pageName.substring(pageName.lastIndexOf('/') + 1);
+
+
+        menuItems.find(function(item) {
+            if (item.link == pageName) {
+                item.isActive = true;
+            }
+        });
+
+        // Return our menuItems to the component
+        return { menuItems };
     },
     template: `
     <div class='header'>
         <h1 class='header-title'> {{ name }} </h1>
         <div class='nav-bar'>
-            <navbar-items v-for="item in items" v-bind:item="item" v-bind:href="item.link"></navbar-items>
+            <navbar-items v-for="item in menuItems" v-bind:item="item" v-bind:href="item.link" v-bind:class="{'active':(item.isActive)}"></navbar-items>
         </div>
     </div>`
 })
