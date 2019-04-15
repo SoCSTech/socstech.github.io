184f3d1c814e545c6186939aac86c8fb3d984130
Tom Reed <t.reed1104@gmail.com>
2019-04-15 17:04:14 +0100

    amended the components css selectors

--- socstech.js
+++ socstech.js
@@ -59,8 +59,8 @@ Vue.component('staff-profile-component', {
         <h1 class='staff-name'> {{ staff.name }} </h1>
         <h2 class='staff-role'> Role: {{ staff.role }} </h2>
         <h2 class='staff-role' v-if='staff.speciality !== ""'> Specialisation: {{ staff.speciality }} </h2>
-        <a class='profile-link' v-if='staff.staff_profile !== ""' v-bind:href='staff.staff_profile'> {{ staff.staff_profile }} </a>
-        <a class='github-link' v-if='staff.github !== ""' v-bind:href='staff.github'> {{ staff.github }} </a>
+        <a class='link-icon' v-if='staff.staff_profile !== ""' v-bind:href='staff.staff_profile'> {{ staff.staff_profile }} </a>
+        <a class='link-icon' v-if='staff.github !== ""' v-bind:href='staff.github'> {{ staff.github }} </a>
     </div>`
 });
 
